b345e1a54d813486f0eca26c38178cb37ffea0a1
Ed Andersen <eandersen@mdsol.com>
2014-04-07 15:03:50 +0900

    Removed NUnit Addin as TeamCity was not having it.

--- tests/Crichton.Representors.Tests/LocalAddin.cs
+++
@@ -1,9 +0,0 @@
-using NUnit.Core.Extensibility;
-
-namespace Crichton.Representors.Tests
-{
-    [NUnitAddin]
-    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
-    {   
-    }
-}
--- tests/Crichton.Representors.Tests/Serializers/HalSerializerTests.cs
+++ tests/Crichton.Representors.Tests/Serializers/HalSerializerTests.cs
@@ -6,30 +6,34 @@ using System.Threading.Tasks;
 using Crichton.Representors.Serializers;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
-using Ploeh.AutoFixture.NUnit2;
+using Ploeh.AutoFixture;
 
 namespace Crichton.Representors.Tests.Serializers
 {
     public class HalSerializerTests : TestBase
     {
         private HalSerializer sut;
+        private CrichtonRepresentor representor;
+        private IFixture fixture;
 
         [TestFixtureSetUp]
         public void Init()
         {
             sut = new HalSerializer();
+            fixture = GetFixture();
+            representor = fixture.Create<CrichtonRepresentor>();
         }
 
-        [Test, AutoData]
-        public void SelfLinkIsSet(CrichtonRepresentor representor)
+        [Test]
+        public void SelfLinkIsSet()
         {
             var result = JObject.Parse(sut.Serialize(representor));
 
             Assert.AreEqual(result["_links"]["self"].Value<string>("href"), representor.SelfLink);
         }
 
-        [Test, AutoData]
-        public void ThrowsExceptionIfNoSelfLinkSet(CrichtonRepresentor representor)
+        [Test]
+        public void ThrowsExceptionIfNoSelfLinkSet()
         {
             representor.SelfLink = null;
 
