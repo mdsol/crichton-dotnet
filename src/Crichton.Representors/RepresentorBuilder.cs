b5eb351109ed4fb43ea209b0cc095475ad635b42
Ed Andersen <eandersen@mdsol.com>
2014-04-10 09:20:25 +0900

    RepresentorBuilder created with methods suitable for HAL Deserializer

---
+++ src/Crichton.Representors/RepresentorBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Crichton.Representors
+{
+    public class RepresentorBuilder
+    {
+        private readonly CrichtonRepresentor representor;
+
+        public RepresentorBuilder()
+        {
+            representor = new CrichtonRepresentor();
+        }
+
+        public CrichtonRepresentor ToRepresentor()
+        {
+            return representor;
+        }
+
+        public void SetSelfLink(string self)
+        {
+            representor.SelfLink = self;
+        }
+
+        public void SetAttributes(JObject attributes)
+        {
+            representor.Attributes = attributes;
+        }
+
+        public void SetAttributesFromObject(object data)
+        {
+            representor.Attributes = JObject.FromObject(data);
+        }
+
+        public void AddTransition(string rel, string uri)
+        {
+            representor.Transitions.Add(new CrichtonTransition() { Rel = rel, Uri = uri});
+        }
+    }
+}
---
+++ tests/Crichton.Representors.Tests/RepresentorBuilderTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Crichton.Representors.Tests
+{
+    public class RepresentorBuilderTests : TestWithFixture
+    {
+        private RepresentorBuilder sut;
+
+        [SetUp]
+        public void Init()
+        {
+            sut = new RepresentorBuilder();
+            Fixture = GetFixture();
+        }
+
+        [Test]
+        public void ToRepresentor_ReturnsARepresentor()
+        {
+            var result = sut.ToRepresentor();
+
+            Assert.IsInstanceOf<CrichtonRepresentor>(result);
+        }
+
+        [Test]
+        public void SetSelfLink_SetsSelfLink()
+        {
+            var self = Fixture.Create<string>();
+
+            sut.SetSelfLink(self);
+            var result = sut.ToRepresentor();
+
+            Assert.AreEqual(self, result.SelfLink);
+        }
+
+        [Test]
+        public void SetAttributes_SetsRepresentorAttributes()
+        {
+            var attributes = new JObject();
+            sut.SetAttributes(attributes);
+            var result = sut.ToRepresentor();
+
+            Assert.AreEqual(attributes, result.Attributes);
+        }
+
+        [Test]
+        public void SetAttributesFromObject_SetsAttributes()
+        {
+            var example = Fixture.Create<ExampleDataObject>();
+            var expectedJObject = JObject.FromObject(example);
+
+            sut.SetAttributesFromObject(example);
+            var result = sut.ToRepresentor();
+
+            foreach (var property in expectedJObject.Properties())
+            {
+                Assert.AreEqual(expectedJObject[property.Name], result.Attributes[property.Name]);
+            }
+
+        }
+
+        [Test]
+        public void AddTransition_CorrectlyAddsSimpleTransition()
+        {
+            var rel = Fixture.Create<string>();
+            var uri = Fixture.Create<string>();
+
+            sut.AddTransition(rel, uri);
+            var result = sut.ToRepresentor();
+
+            result.Transitions.Should().Contain(t => t.Rel == rel && t.Uri == uri);
+
+        }
+    
+    }
+}
