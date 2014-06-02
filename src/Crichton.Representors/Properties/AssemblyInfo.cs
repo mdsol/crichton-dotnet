456885256a8643158fbd7adf190a72c385a7f552
Ed Andersen <eandersen@mdsol.com>
2014-06-02 12:28:36 +0900

    Bump nuget version number to 0.1.2, includes HaleSerializer

--- src/Crichton.Representors/Properties/AssemblyInfo.cs
+++ src/Crichton.Representors/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyFileVersionAttribute("0.1.1")]
-[assembly: AssemblyInformationalVersion("0.1.1-alpha")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersionAttribute("0.1.2")]
+[assembly: AssemblyInformationalVersion("0.1.2-alpha")]
