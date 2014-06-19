8278d3bf14b26f389b773105981eb884b8d2214d
Kenya Matsumoto <kmatsumoto@mdsol.com>
2014-06-19 13:49:52 +0900

    Added Crichton.Representors.Benchmark.

---
+++ tools/Crichton.Representors.Benchmark/run.bat
@@ -0,0 +1,2 @@
+.\bin\Debug\Crichton.Representors.Benchmark.exe deserializer --iterations 5 --filepath "C:\GitProjects\crichton-dotnet\tests\Crichton.Representors.Tests\Integration\TestData\Hal\AllLinkObjectProperties.json"
+pause
