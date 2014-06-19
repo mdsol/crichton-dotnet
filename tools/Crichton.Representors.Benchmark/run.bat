558284be319ad91a61d2586b4c2fa8f2acdf60a1
Kenya Matsumoto <kmatsumoto@mdsol.com>
2014-06-19 16:30:18 +0900

    Modified feedbacks from Ed. * be in the root of the repository, maybe called "benchmark.bat" * actually compile the project with MSBuild in Release mode before running the exe * Should be a relative path here to the JSON instead of absolute * Also lets put the benchmark tool project in a Solution Folder called "tools" in VS. Added README.md for Benchmark.

--- tools/Crichton.Representors.Benchmark/run.bat
+++
@@ -1,2 +0,0 @@
-.\bin\Debug\Crichton.Representors.Benchmark.exe deserializer --iterations 5 --filepath "C:\GitProjects\crichton-dotnet\tests\Crichton.Representors.Tests\Integration\TestData\Hal\AllLinkObjectProperties.json"
-pause
