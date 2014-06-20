85ea49317fa83b4cc0fd7c4a4907321dcb1d9bb6
Ed Andersen <eandersen@mdsol.com>
2014-06-20 15:46:23 +0900

    Base project foundation for Crichton.Client

---
+++ src/Crichton.Client/CrichtonClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Crichton.Representors.Serializers;
+
+namespace Crichton.Client
+{
+    public class CrichtonClient
+    {
+        public HttpClient HttpClient { get; private set; }
+        public ISerializer Serializer { get; private set; }
+        public Uri BaseUri { get; private set; }
+
+        public CrichtonClient(HttpClient httpClient, Uri baseUri, ISerializer serializer)
+        {
+            HttpClient = httpClient;
+            Serializer = serializer;
+            BaseUri = baseUri;
+        }
+    }
+}
---
+++ tests/Crichton.Client.Tests/TestWithFixture.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoRhinoMock;
+
+namespace Crichton.Client.Tests
+{
+    public class TestWithFixture
+    {
+        internal IFixture Fixture;
+
+        public IFixture GetFixture()
+        {
+            var fixture = new Fixture().Customize(new MultipleCustomization()).Customize(new AutoRhinoMockCustomization());
+            fixture.Behaviors.Remove(fixture.Behaviors.OfType<ThrowingRecursionBehavior>().Single());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            return fixture;
+        }
+    }
+}
