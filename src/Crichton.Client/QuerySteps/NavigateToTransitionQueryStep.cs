1b8bd940bfd66246b06706826bcb256f834cc905
Kenya Matsumoto <kmatsumoto@mdsol.com>
2014-07-01 15:23:46 +0900

    Added guard clauses into Crichton.Client. Removed unused using statements.

--- src/Crichton.Client/QuerySteps/NavigateToTransitionQueryStep.cs
+++ src/Crichton.Client/QuerySteps/NavigateToTransitionQueryStep.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Crichton.Representors;
 
@@ -13,16 +11,23 @@ namespace Crichton.Client.QuerySteps
 
         public NavigateToTransitionQueryStep(string rel)
         {
+            if (rel == null) { throw new ArgumentNullException("rel"); }
+
             selectionFunc = transition => transition.Rel == rel;
         }
 
         public NavigateToTransitionQueryStep(Func<CrichtonTransition, bool> transitionSelectionFunc)
         {
+            if (transitionSelectionFunc == null) { throw new ArgumentNullException("transitionSelectionFunc"); }
+
             selectionFunc = transitionSelectionFunc;
         }
 
         public Task<CrichtonRepresentor> ExecuteAsync(CrichtonRepresentor currentRepresentor, ITransitionRequestHandler transitionRequestHandler)
         {
+            if (currentRepresentor == null) { throw new ArgumentNullException("currentRepresentor"); }
+            if (transitionRequestHandler == null) { throw new ArgumentNullException("transitionRequestHandler"); }
+
             var transition = LocateTransition(currentRepresentor);
 
             return transitionRequestHandler.RequestTransitionAsync(transition);
@@ -30,6 +35,8 @@ namespace Crichton.Client.QuerySteps
 
         public CrichtonTransition LocateTransition(CrichtonRepresentor currentRepresentor)
         {
+            if (currentRepresentor == null) { throw new ArgumentNullException("currentRepresentor"); }
+
             var transition = currentRepresentor.Transitions.Single(selectionFunc);
             return transition;
         }
