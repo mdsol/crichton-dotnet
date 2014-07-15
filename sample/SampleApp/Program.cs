04dac456b52376b48ee989af516043efe41a6684
Kenya Matsumoto <kmatsumoto@mdsol.com>
2014-07-15 14:16:34 +0900

    Added sample app.

---
+++ sample/SampleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using Crichton.Client;
+using Crichton.Representors.Serializers;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCrichtonClient(args[0], args[1]);
+        }
+
+        private static void TestCrichtonClient(string baseUrl, string relativeUrl)
+        {
+            var httpClient = new HttpClient() { BaseAddress = new Uri(baseUrl) };
+
+            var serializer = new JsonSerializer();
+            var crichtonClient = new CrichtonClient(httpClient, serializer);
+            var query = crichtonClient.CreateQuery().WithUrl(relativeUrl);
+            var representor = crichtonClient.ExecuteQueryAsync(query).Result;
+
+            //
+            // get data from the representor.
+            // 
+            
+            Console.WriteLine(serializer.Serialize(representor));
+        }
+    }
+}
